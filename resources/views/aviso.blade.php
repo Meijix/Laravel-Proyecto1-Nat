5b3f084faa4648ecbd8a7911b44f60704d2a2eea
Meijix <nmejia30@ciencias.unam.mx>
2025-01-23 10:08:08 -0600

    Update aviso view to include conditional messaging

--- resources/views/aviso.blade.php
+++ resources/views/aviso.blade.php
@@ -9,15 +9,22 @@
 </head>
 <body>
     <h1>Aviso de curso de inducción</h1>
-    <p>Bienvenidos estudiantes de la generación 2025 a la Facultad de Contaduría y Administración de la UNAM.</p>
 
     <p>Se les convoca a asistir al curso de inducción, que se realizará durante la semana del  27 al 31 de enero de 2025 en el Auditorio Carlos Pérez del Toro a partir de las 9:00 am.</p>
 
-    @if($estudiante_informatica = 1)
+    <?php
+        $estudiante_informatica = 4;
+    ?>
+
+    @if($estudiante_informatica == 1)
         <h2>Aviso conferencia</h2>
         <p>Se convoca a todos los estudiantes de informática de nuevo ingreso a participar en la conferencia</p>
 
         <p>-La informática como apoyo estratégico en las organizaciones- misma que se impartirá el 6 de febrero de 2025 en el Aula Magna de la Facultad a las 14:00 hrs</p>
+    @else
+        <h2>Aviso conferencia</h2>
+        <p>Si les interesan otras conferencias ver el anuncio oficial. </p>
+
     @endif
 </body>
 </html>
