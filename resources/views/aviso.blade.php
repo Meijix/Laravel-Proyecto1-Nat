b63f7e658751a9fd056c70667e415d02ae32a7c4
Meijix <nmejia30@ciencias.unam.mx>
2025-01-23 10:51:30 -0600

    Remove incorrect stylesheet link from aviso view

--- resources/views/aviso.blade.php
+++ resources/views/aviso.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('resources/css/style.css') }}">
     <title>Aviso</title>
 </head>
 <body>
