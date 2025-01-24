df753597deb1c75d029e13776d57da237a18b0a6
Meijix <nmejia30@ciencias.unam.mx>
2025-01-23 20:04:20 -0600

    Refactor Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
-abstract class Controller
+class Controller extends BaseController
 {
-    //
+    use ValidatesRequests;
 }
--- resources/views/agregar.blade.php
+++ resources/views/agregar.blade.php
@@ -66,7 +66,8 @@
         </div>
 
         <!-- Submit button -->
-        <button data-mdb-ripple-init type="button" class="btn btn-primary btn-block mb-4">Agregar curso</button>
+
+        <button data-mdb-ripple-init type="submit" class="btn btn-primary btn-block mb-4">Agregar curso</button>
     </form>
 
 </section>
