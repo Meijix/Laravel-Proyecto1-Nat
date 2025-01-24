acfc3d6ad7a55f32db3f79bd6f09c7e68a287828
Meijix <nmejia30@ciencias.unam.mx>
2025-01-23 20:20:41 -0600

    Add authorization and validation traits to Controller; define table and fillable fields in Curso model

--- app/Models/Curso.php
+++ app/Models/Curso.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Curso extends Model
 {
-    //
+    protected $table = 'cursos';
+    protected $fillable = ['nombre', 'objetivo', 'modalidad', 'cupo', 'periodo', 'horario', 'dias', 'salon'];
+    
 }
