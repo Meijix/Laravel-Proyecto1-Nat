8c51f2bb394a1dfc5646a2c68bf51cdc6215d569
Meijix <nmejia30@ciencias.unam.mx>
2025-01-29 20:17:39 -0600

    Update edit course view with pre-filled fields and change button text

--- resources/views/editar.blade.php
+++ resources/views/editar.blade.php
@@ -3,19 +3,19 @@
 @section('contenido')
 <!-- Contenido principal -->
 <section class="content">
-    <h1>Agregar Nuevo Curso</h1>
+    <h1>Editar Curso</h1>
  {{--    <p>Por favor, llena el siguiente formulario para agregar un nuevo curso:</p> --}}
     <!-- Formulario para agregar un curso -->
-    <form action="{{route('store')}}" method="POST">
+    <form action="{{route('update', $curso->id)}}" method="POST">
         @csrf
         <!-- 1 column -->
         <div data-mdb-input-init class="form-outline mb-4">
-            <input type="text" id="nombre" class="form-control" />
+            <input type="text" id="nombre" class="form-control" value="{{$curso->nombre}}" />
             <label class="form-label" for="nombre">Nombre:</label>
         </div>
 
         <div data-mdb-input-init class="form-outline mb-4">
-            <input type="text" id="objetivo" class="form-control" />
+            <input type="text" id="objetivo" class="form-control" value="{{$curso->objetivo}}" />
             <label class="form-label" for="objetivo">Objetivo:</label>
         </div>
 
@@ -23,13 +23,13 @@
         <div class="row mb-4">
             <div class="col">
             <div data-mdb-input-init class="form-outline">
-                <input type="text" id="modalidad" class="form-control" />
+                <input type="text" id="modalidad" class="form-control" value="{{$curso->modalidad}}" />
                 <label class="form-label" for="modalidad">Modalidad:</label>
             </div>
             </div>
             <div class="col">
             <div data-mdb-input-init class="form-outline">
-                <input type="text" id="cupo" class="form-control" />
+                <input type="text" id="cupo" class="form-control" value="{{$curso->cupo}}" />
                 <label class="form-label" for="cupo">Cupo:</label>
             </div>
             </div>
@@ -38,13 +38,13 @@
         <div class="row mb-4">
             <div class="col">
             <div data-mdb-input-init class="form-outline">
-                <input type="text" id="periodo" class="form-control" />
+                <input type="text" id="periodo" class="form-control" value="{{$curso->periodo}}" />
                 <label class="form-label" for="periodo">Periodo:</label>
             </div>
             </div>
             <div class="col">
             <div data-mdb-input-init class="form-outline">
-                <input type="text" id="horario" class="form-control" />
+                <input type="text" id="horario" class="form-control" value="{{$curso->horario}}"/>
                 <label class="form-label" for="horario">Horario:</label>
             </div>
             </div>
@@ -53,13 +53,13 @@
         <div class="row mb-4">
             <div class="col">
             <div data-mdb-input-init class="form-outline">
-                <input type="text" id="dia" class="form-control" />
+                <input type="text" id="dia" class="form-control" value="{{$curso->dias}}" />
                 <label class="form-label" for="dia">Dia:</label>
             </div>
             </div>
             <div class="col">
             <div data-mdb-input-init class="form-outline">
-                <input type="text" id="salon" class="form-control" />
+                <input type="text" id="salon" class="form-control" value="{{$curso->salon}}" />
                 <label class="form-label" for="salon">Salon:</label>
             </div>
             </div>
@@ -67,7 +67,7 @@
 
         <!-- Submit button -->
 
-        <button data-mdb-ripple-init type="submit" class="btn btn-primary btn-block mb-4">Agregar curso</button>
+        <button data-mdb-ripple-init type="submit" class="btn btn-primary btn-block mb-4">Actualizar curso</button>
     </form>
 
 </section>
